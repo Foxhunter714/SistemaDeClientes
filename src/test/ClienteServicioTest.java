4524ed02615c9b67666b6cfca06ab79ad2947e52
foxhunter <kevin.floba@gmail.com>
2021-01-08 17:39:54 -0300

    Sistema de Clientes para la carrera de Full-Stack Java

--- src/test/ClienteServicioTest.java
+++ src/test/ClienteServicioTest.java
@@ -9,7 +9,7 @@ import model.CategoriaEnum;
 import model.Cliente;
 import servicios.ClienteServicio;
 
-public class AgregarClienteNullTest {
+public class ClienteServicioTest {
 
 	private ClienteServicio clienteServicio;
 	private Cliente cliente;
@@ -21,7 +21,26 @@ public class AgregarClienteNullTest {
 	}
 
 	@Test
-	public void listarClienteTest() {
+	public void agregarClienteTest() {
+		// Given
+		String runCliente = "18871922-1";
+		String nombreCliente = "Alejandro";
+		String apellidoCliente = "Garcia";
+		String aniosCliente = "23";
+		CategoriaEnum nombreCategoria = CategoriaEnum.ACTIVO;
+		// When
+
+		clienteServicio.agregarCliente(runCliente, nombreCliente, apellidoCliente, aniosCliente, nombreCategoria);
+		cliente = clienteServicio.getListaClientes().get(0);
+		String a = cliente.getNombreCliente();
+		System.out.print(a);
+
+		// Then
+		assertEquals(a, "Alejandro");
+	}
+
+	@Test
+	public void agregarClienteNuloTest() {
 		// Given
 		String runCliente = "";
 		String nombreCliente = "";
